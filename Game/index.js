49545ea77509be17df49ae941c4e87437aff7505
sanauwar <sanauwarnadeem123@gmail.com>
2022-10-03 03:41:14 +0530

    Comment the code:

--- Game/index.js
+++ Game/index.js
@@ -1,16 +1,17 @@
 let queue = [];
-document.addEventListener('click', () => {
+document.addEventListener('click', (event) => {
   const elem = event.target;
   if (!elem.classList.contains('tile')) {
     return false;
   }
   if (queue.includes(elem)) return false;
+
   elem.classList.add("active")
   queue.push(elem)
   if (queue.length >= 9) {
     const interval = setInterval(() => {
       let tile = queue.shift()
-      alert(tile.dataset.id)
+      //alert(tile.dataset.id)
       tile.classList.remove("active")
       if (queue.length === 0) {
         clearInterval(interval);
